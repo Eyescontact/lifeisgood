741bc7619058e754259275074d50c82c8f7ca88b
Eyescontact <623103418@qq.com>
2017-05-26 12:31:54 +0800

    |---2017年05月26日12:31 feeling push

--- dayterminal.sh
+++ dayterminal.sh
@@ -17,7 +17,7 @@ isolation="|"
 
 sed -i '3i\ <br /> '"$str"' <br />' Dayfeelingline.md
 sed -i '3i\ <br /> '"$send"'' Dayfeelingline.md
-sed -i '3i\ <br /> '"$isolation"'' Dayfeelingline.md
+sed -i '3i\ '"$isolation"'' Dayfeelingline.md
 
 git add -A
 git commit -m "$send feeling push"
